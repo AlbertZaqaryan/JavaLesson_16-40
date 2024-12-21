ab07a96d326aaa52d1dc19ce13be4c7e53b84274
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2024-12-21 18:29:17 +0400

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(5 + 4);
+
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+//        System.out.print("Enter day: ");
+//        int day = scan.nextInt();
+//        System.out.print("Enter month: ");
+//        String month = scan.next();
+//        System.out.print("Enter year: ");
+//        int year = scan.nextInt();
+//        HashMap<String, Integer> map = new HashMap<>();
+//        map.put("Hunvar", 1);
+//        map.put("Petrvar", 2);
+//        map.put("Mart", 3);
+//        map.put("April", 4);
+//        map.put("Mayis", 5);
+//        map.put("Hunis", 6);
+//        map.put("Hulis", 7);
+//        map.put("Ogostos", 8);
+//        map.put("September", 9);
+//        map.put("Hoktember", 10);
+//        map.put("Noyember", 11);
+//        map.put("Dektember", 12);
+//        System.out.println(day * map.get(month) == year % 100);
+        // ------------------------------------------------------
+//        String text = scan.nextLine();
+//        ArrayList<Character> arr = new ArrayList<>();
+//        for(int i = 0; i < text.length(); i ++){
+//            arr.add(text.charAt(i));
+//        }
+//        ArrayList<Character> newarr = new ArrayList<>();
+//        while(!arr.isEmpty()){
+//            newarr.add(arr.getFirst());
+//            newarr.add(arr.getLast());
+//            arr.remove(arr.getFirst());
+//            arr.remove(arr.getLast());
+//        }
+//        System.out.println(newarr);
+        // ------------------------------------------------------
+
     }
 }
