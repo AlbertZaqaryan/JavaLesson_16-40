c0a7d724f1093cbc2348772f8da175e7adc99952
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2024-12-24 18:25:13 +0400

    functions'

--- src/Main.java
+++ src/Main.java
@@ -5,45 +5,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
-        Random rand = new Random();
-
-//        System.out.print("Enter day: ");
-//        int day = scan.nextInt();
-//        System.out.print("Enter month: ");
-//        String month = scan.next();
-//        System.out.print("Enter year: ");
-//        int year = scan.nextInt();
-//        HashMap<String, Integer> map = new HashMap<>();
-//        map.put("Hunvar", 1);
-//        map.put("Petrvar", 2);
-//        map.put("Mart", 3);
-//        map.put("April", 4);
-//        map.put("Mayis", 5);
-//        map.put("Hunis", 6);
-//        map.put("Hulis", 7);
-//        map.put("Ogostos", 8);
-//        map.put("September", 9);
-//        map.put("Hoktember", 10);
-//        map.put("Noyember", 11);
-//        map.put("Dektember", 12);
-//        System.out.println(day * map.get(month) == year % 100);
-        // ------------------------------------------------------
-//        String text = scan.nextLine();
-//        ArrayList<Character> arr = new ArrayList<>();
-//        for(int i = 0; i < text.length(); i ++){
-//            arr.add(text.charAt(i));
-//        }
-//        ArrayList<Character> newarr = new ArrayList<>();
-//        while(!arr.isEmpty()){
-//            newarr.add(arr.getFirst());
-//            newarr.add(arr.getLast());
-//            arr.remove(arr.getFirst());
-//            arr.remove(arr.getLast());
-//        }
-//        System.out.println(newarr);
-        // ------------------------------------------------------
-
+        System.out.println(next_prime(19));
     }
+
+    static boolean is_prime(int number){
+        boolean state = true;
+        for(int i = 2; i < number; i ++){
+            if(number % i == 0){
+                state = false;
+            }
+        }
+        return state;
+    }
+
+    static int next_prime(int number){
+        number ++;
+        while(!is_prime(number)){
+            number++;
+        }
+        return number;
+    }
+
+
 }
